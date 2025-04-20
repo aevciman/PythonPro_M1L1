33bb0e259fe025ac476cfd4f5fc2b4b5948ff15e
aevciman14 <alimeteevciman14@gmail.com>
2025-04-20 15:21:35 +0300

    Create meme_sozluk.py

---
+++ meme_sozluk.py
@@ -0,0 +1,13 @@
+meme_sozluk= {"LOL":"Komik bir şeye verilen cevap.",
+              "CRINGE":"Garip ya da utandırıcı bir şey.",
+              "ROFL":"Bir şakaya karşılık cevap.",
+              "SHEESH":"Onaylamamak.",
+              "CREEPY":"Korkunç.",
+              "AGGRO":"Agresifleşmek/sinirlenmek"}
+while True:  
+    kelime=input("Bilmediğiniz kelimeyi giriniz:").upper()
+
+    if kelime in meme_sozluk:
+        print(kelime,"=",meme_sozluk[kelime])
+    else:
+        print("Maalesef, bu kelime sözlüğümüzde bulunmuyor:(")
